39fdf0a077d87d201d02090e0d0a04d681fe85a3
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-09 16:16:25 +0200

    Fancy Grid

---
+++ fancygrid/src/fancyGridExample.js
@@ -0,0 +1,62 @@
+import FancyGrid from 'fancygrid';
+
+class FancyGridExample extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	render() {
+		const elData = JSON.parse(this.getAttribute('elementData'));
+
+		this.innerHTML = `<div id="container"></div>`;
+
+		document.addEventListener('DOMContentLoaded', function() {
+			new FancyGrid({
+				renderTo: 'container',
+				width: 500,
+				height: 400,
+				data: elData,
+				columns: [
+					{
+						index: 'company',
+						title: 'Company',
+						type: 'string',
+						width: 100,
+					},
+					{
+						index: 'name',
+						title: 'Name',
+						type: 'string',
+						width: 100,
+					},
+					{
+						index: 'surname',
+						title: 'Sur Name',
+						type: 'string',
+						width: 100,
+					},
+					{
+						index: 'age',
+						title: 'Age',
+						type: 'number',
+						width: 100,
+					},
+				],
+			});
+		});
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	static get observedAttributes() {
+		return ['elementData'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.render();
+	}
+}
+
+window.customElements.define('fancy-grid', FancyGridExample);
