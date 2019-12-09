cad2a41113ba57a6afdc73e7c0a57a124eefa9ad
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-09 16:20:52 +0200

    Update

--- fancygrid/src/fancyGridExample.js
+++ fancygrid/src/fancyGridExample.js
@@ -10,39 +10,37 @@ class FancyGridExample extends HTMLElement {
 
 		this.innerHTML = `<div id="container"></div>`;
 
-		document.addEventListener('DOMContentLoaded', function() {
-			new FancyGrid({
-				renderTo: 'container',
-				width: 500,
-				height: 400,
-				data: elData,
-				columns: [
-					{
-						index: 'company',
-						title: 'Company',
-						type: 'string',
-						width: 100,
-					},
-					{
-						index: 'name',
-						title: 'Name',
-						type: 'string',
-						width: 100,
-					},
-					{
-						index: 'surname',
-						title: 'Sur Name',
-						type: 'string',
-						width: 100,
-					},
-					{
-						index: 'age',
-						title: 'Age',
-						type: 'number',
-						width: 100,
-					},
-				],
-			});
+		new FancyGrid({
+			renderTo: 'container',
+			width: 500,
+			height: 400,
+			data: elData,
+			columns: [
+				{
+					index: 'company',
+					title: 'Company',
+					type: 'string',
+					width: 100,
+				},
+				{
+					index: 'name',
+					title: 'Name',
+					type: 'string',
+					width: 100,
+				},
+				{
+					index: 'surname',
+					title: 'Sur Name',
+					type: 'string',
+					width: 100,
+				},
+				{
+					index: 'age',
+					title: 'Age',
+					type: 'number',
+					width: 100,
+				},
+			],
 		});
 	}
 
