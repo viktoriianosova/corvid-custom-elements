f7e2d493ea318868397b8380f72d0b7fe51194dd
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-09 17:40:48 +0200

    Add Plain Html

---
+++ plain-html-component/src/customHtmlCompoment.js
@@ -0,0 +1,25 @@
+class CustomHtmlComponent extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	render() {
+		const componentCode = JSON.parse(this.getAttribute('componentCode'));
+
+		this.innerHTML = `<div id="container"><style>${componentCode.cssCode}</style>${componentCode.htmlCode}</div>`;
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	static get observedAttributes() {
+		return ['componentCode'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.render();
+	}
+}
+
+window.customElements.define('custom-html-component', CustomHtmlComponent);
