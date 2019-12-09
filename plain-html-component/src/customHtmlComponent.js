b56e605e54437d722c83278a6a2edb5bd6416c7e
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-09 18:24:34 +0200

    Fix wrapper

--- plain-html-component/src/customHtmlComponent.js
+++ plain-html-component/src/customHtmlComponent.js
@@ -6,7 +6,7 @@ class CustomHtmlComponent extends HTMLElement {
 	render() {
 		const componentCode = JSON.parse(this.getAttribute('componentCode'));
 
-		this.innerHTML = `<div id="container" style="height:100vh; width: 100%"><style>${componentCode.cssCode}</style>${componentCode.htmlCode}</div>`;
+		this.innerHTML = `<div id="container" style="height:inherit; width: 100%"><style>${componentCode.cssCode}</style>${componentCode.htmlCode}</div>`;
 	}
 
 	connectedCallback() {
