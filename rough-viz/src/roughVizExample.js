8da306873257fd98778824907a047db2b2e077de
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-09 14:59:39 +0200

    Add rough-viz

---
+++ rough-viz/src/roughVizExample.js
@@ -0,0 +1,42 @@
+import roughViz from 'rough-viz';
+
+class RoughVizChart extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	render() {
+		// const elData = JSON.parse(this.getAttribute('elementData'));
+		this.innerHTML = `<div id="vis1"></div>`;
+
+		new roughViz.Donut({
+			element: '#vis1',
+			data: {
+				labels: ['North', 'South', 'East', 'West'],
+				values: [10, 5, 8, 3],
+			},
+			title: 'Regions',
+			width: window.innerWidth / 2,
+			roughness: 8,
+			colors: ['red', 'orange', 'blue', 'skyblue'],
+			stroke: 'black',
+			strokeWidth: 3,
+			fillStyle: 'cross-hatch',
+			fillWeight: 3.5,
+		});
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	// static get observedAttributes() {
+	// 	return ['elementData'];
+	// }
+
+	// attributeChangedCallback(name, oldValue, newValue) {
+	// 	this.render();
+	// }
+}
+
+window.customElements.define('rough-viz-chart', RoughVizChart);
