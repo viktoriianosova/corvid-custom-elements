a834ed864c9a4361f2f92edb4b054c5014efd714
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-12-10 11:55:50 +0200

    Typo

--- plain-html-component/src/customHtmlComponent.js
+++ plain-html-component/src/customHtmlComponent.js
@@ -6,8 +6,8 @@ class CustomHtmlComponent extends HTMLElement {
 	render() {
 		const componentCode = JSON.parse(this.getAttribute('componentCode'));
 
-		const htmlCode = componentCode.htmlCode.replace('/n', '');
-		const cssCode = componentCode.cssCode.replace('/n', '');
+		const htmlCode = componentCode.htmlCode.replace('\n', '');
+		const cssCode = componentCode.cssCode.replace('\n', '');
 
 		this.innerHTML = `<div id="container" style="height:inherit; width: 100%"><style>${cssCode}</style>${htmlCode}</div>`;
 	}
