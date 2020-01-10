7096d4605006e94f1fb5c8b2ebaa4986539ba397
Viktoriia Nosova <viktoriianosova@hotmail.com>
2020-01-10 15:11:48 +0200

    Added icons to submenu

--- toolstrip/src/toolstrip.js
+++ toolstrip/src/toolstrip.js
@@ -1,23 +1,21 @@
-import printerImg from '../assets/printer-black.svg';
-
 class Toolstrip extends HTMLElement {
 	constructor() {
 		super();
 	}
 
 	render() {
-		console.log(printerImg);
-
 		this.innerHTML = `
         <style>
             * {
                 margin: 0;
                 padding: 0;
                 box-sizing: border-box;
+                font-family: Tahoe;
+                font-size: 15px;
             }
 
             #wrapper {
-
+                position: relative;
                 height: 50px;
                 border: 1px solid lightgrey;
                 background-color: #F0F0F0;
@@ -42,30 +40,33 @@ class Toolstrip extends HTMLElement {
             }
 
             .button-box {
-                font-family: Tahoe;
-                font-size: 14px;
+                height: 35px;
                 display: flex;
-                justify-content: space-around;
+                justify-content: space-evenly;
                 align-items: center;
-                border: 1px solid grey;
+                border: 1px solid;
+                border-color: rgb(169,169,169, 0);
             }
 
             .button-box:hover {
-                border: 1px solid grey;
+                border-color: rgb(169,169,169, 1);
+            }
+
+            .button-box-clicked {
+                color: white;
+                background-color: grey;
+            }
+
+            .button-box-clicked img {
+                filter: brightness(0) invert(1);
             }
 
             .button-regular {
                 min-width: 70px;
-                height: 35px;
-                background: none;
-                border: none;
-                font-family: Tahoe;
-                font-size: 14px;
             }
 
             .square-button {
                 width: 35px;
-                height: 35px;
             }
 
             #font-family {
@@ -99,39 +100,77 @@ class Toolstrip extends HTMLElement {
                 height: 8px;
             }
 
-            .icon:hover {
+            #file-submenu {
+                position: absolute;
+                left: 20px;
+                top: 45px;
+                width: 200px;
+                min-height: 200px;
+                border: 1px solid lightgrey;
+                background-color: white;
+            }
+
+            .file-submenu-section:not(:last-child) {
+                border-bottom: 1px solid lightgrey;
+            }
+            
+            .file-submenu-item {
+                height: 50px;
+                display: flex;
+                align-items: center;
+                padding: 5px 10px;
+            }
+
+            .file-submenu-item .icon-left {
+                margin-right: 10px;
+            }
+
+            .file-submenu-item .action-name {
+                flex: 1;
+            }
+
+            .file-submenu-item .keyboard-shortcut {
+                color: grey;
+                margin-right: 10px;
+            }
+
+            .file-submenu-item:hover {
+                background-color: #147FCC;
+                color: white;
+            }
+
+            .file-submenu-item:hover img {
                 filter: brightness(0) invert(1);
             }
 
+            .file-submenu-item:hover .keyboard-shortcut {
+                color: white;
+            }
         </style>
         <div id="wrapper">
             <div class="section-wrapper">
-                <div class="file button-box">
-                    <button class="button-regular">
-                        <span>File</span>
-                        <img class="icon-small" src="https://image.flaticon.com/icons/svg/58/58979.svg"/>
-                    </button>
+                <div class="button-box button-regular button-box-clicked">
+                    <span>File</span>
+                    <img class="icon-small" src="https://image.flaticon.com/icons/svg/58/58979.svg"/>
                 </div>
             </div>
 
             <div class="section-wrapper">
-                <div class="print button-box">
-                    <button class="button-regular">
-    
-                        <span>Print</span>
-                    </button>
+                <div class="button-box button-regular button-box-clicked">
+                    <img class="icon" src="https://image.flaticon.com/icons/svg/446/446991.svg"/>
+                    <span>Print</span>
                 </div>
             </div>
 
             <div class="section-wrapper mult-buttons-box">
-                <div class="button-box square-button">
+                <div class="button-box square-button button-box-clicked">
                     <img class="icon" src="https://image.flaticon.com/icons/svg/565/565691.svg"/>
                 </div>
                 <div class="button-box square-button">
                     <img class="icon" src="https://image.flaticon.com/icons/svg/565/565698.svg"/>
                 </div>
                 <div class="button-box square-button">
-                <img class="icon" src="https://image.flaticon.com/icons/svg/565/565709.svg"/>
+                <img class="icon" src="https://image.flaticon.com/icons/svg/2087/2087924.svg"/>
                 </div>
             </div>
 
@@ -164,6 +203,53 @@ class Toolstrip extends HTMLElement {
                     <option value="200">200%</option>
                     <option value="fit">Fit</option>
                 </select>
+            </div>
+        </div>
+
+        <div id="file-submenu">
+            <div class="file-submenu-section">
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/1155/1155623.svg"/>
+                    <span class="action-name">New</span>
+                    <span class="keyboard-shortcut">Ctrl+N</span>
+                </div>
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/62/62319.svg"/>
+                    <span class="action-name">Open</span>
+                    <span class="keyboard-shortcut">Ctrl+O</span>
+                </div>
+            </div>
+            <div class="file-submenu-section">
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/60/60721.svg"/>
+                    <span class="action-name">Save</span>
+                    <span class="keyboard-shortcut">Ctrl+S</span>
+                </div>
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/376/376218.svg"/>
+                    <span class="action-name">Save as</span>
+                </div>
+            </div>
+            <div class="file-submenu-section">
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/346/346878.svg"/>
+                    <span class="action-name">Recent documents</span>
+                    <img class="icon-small" src="https://image.flaticon.com/icons/svg/0/320.svg"/>
+                </div>
+            </div>
+            <div class="file-submenu-section">
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/1059/1059157.svg"/>
+                    <span class="action-name">Export as</span>
+                    <img class="icon-small" src="https://image.flaticon.com/icons/svg/0/320.svg"/>
+                </div>
+            </div>
+            <div class="file-submenu-section">
+                <div class="file-submenu-item">
+                    <img class="icon icon-left" src="https://image.flaticon.com/icons/svg/446/446991.svg"/>
+                    <span class="action-name">Print</span>
+                    <span class="keyboard-shortcut">Ctrl+P</span>
+                </div>
             </div>
         </div>
         `;
