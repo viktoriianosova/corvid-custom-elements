eb1615cb879392c102add42503178bde04dbbb8f
Viktoriia Nosova <viktoriianosova@hotmail.com>
2020-01-27 12:32:14 +0200

    Add title input

--- post-component/post-component.js
+++ post-component/post-component.js
@@ -11,12 +11,36 @@ class PostComponent extends HTMLElement {
 		<style>
         @import '//cdn.quilljs.com/1.3.6/quill.core.css';
         @import '//cdn.quilljs.com/1.3.6/quill.snow.css';
-        @import '//cdn.quilljs.com/1.3.6/quill.bubble.css';
+		@import '//cdn.quilljs.com/1.3.6/quill.bubble.css';
+			* {
+				box-sizing: border-box;
+			}
+			#wrapper {
+				width: 600px;
+			}
+			#title {
+				height: 50px;
+				margin-bottom: 20px;
+			}
+			#title input{
+				width: 100%;
+				height: 100%;
+				border: 0;
+				padding: 15px;
+				font-size: 24px;
+				font-family: 'Helvetica Neue';
+				color: grey;
+			}
 		    #editor {
                 height: 400px;
             }
-        </style>
-		<div id="editor"></div>
+		</style>
+		<div id="wrapper">
+			<div id="title">
+				<input type="text" id="title-input" placeholder="Give this post a title"/>
+			</div>
+			<div id="editor"></div>
+		</div>
 		`;
 		const editor = this.shadowRoot.getElementById('editor');
 		const quill = new Quill(editor, {
@@ -34,14 +58,24 @@ class PostComponent extends HTMLElement {
 
 		quill.on('editor-change', function(eventName, ...args) {
 			if (eventName === 'text-change') {
-				const content = quill.container.innerHTML;
-				console.log(content);
+				const richTextContent = quill.container.innerHTML;
+				const text = JSON.stringify(quill.getText());
+				const plainText = text
+					.split('\\n')
+					.join(' ')
+					.split('"')
+					.join(' ')
+					.trim();
+
+				console.log(plainText);
 			}
 		});
 	}
 
 	connectedCallback() {
 		this.render();
+		const titleInput = this.shadowRoot.getElementById('title-input');
+		titleInput.onchange = e => console.log(e.target.value);
 	}
 }
 
